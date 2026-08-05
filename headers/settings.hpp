a3fcbe4b066d4bd5b5959575f96688dadfa1ce0d
Capibara <barbashin.redkern@gmail.com>
2026-08-06 02:51:32 +0300

    Update settings.hpp

--- headers/settings.hpp
+++ headers/settings.hpp
@@ -2,28 +2,74 @@
 #define CALAMIT_SETTINGS_HPP
 
 #include <string>
-
+#include <vector>
+#include <functional>
 #include <raylib.h>
+#include <sol/sol.hpp>
+#include <iostream>
+
+#include "luaConverter.hpp"
 
 namespace calamit {
 
-class Settings {
+template <typename T>
+class Setting {
 public:
-    auto getTargetFps() const { return target_fps; }
-    auto getProjectName() const { return project_name; }
-    auto getWindowSize() const { return window_size; }
-    auto getResizable() const { return resizable; }
+    Setting() = default;
+    Setting(T val) : value(val) {}
 
-    void setTargetFps(int fps) { target_fps = fps; }
-    void setProjectName(const std::string& name) { project_name = name; }
-    void setWindowSize(const Vector2& size) { window_size = size; }
-    void setResizable(bool r) { resizable = r; }
+    T get() const { return value; }
+    void set(const T& val) { value = val; }
+    void set(T&& val) { value = std::move(val); }
+
+    void setFromLua(sol::object obj) {
+        if (obj.valid())
+            value = LuaConverter<T>::convert(obj);
+    }
 
 private:
-    int target_fps = 60;
-    std::string project_name = "CalamitProject";
-    Vector2 window_size = {800.0f, 600.0f};
-    bool resizable = false;
+    T value{};
+};
+
+class Settings {
+public:
+    Settings() { initMappings(); }
+
+    Setting<int> target_fps{60};
+    Setting<std::string> project_name{"CalamitProject"};
+    Setting<Vector2> window_size{{800.0f, 600.0f}};
+    Setting<bool> resizable{false};
+
+    void loadFromLua(const std::string& path) {
+        sol::state lua;
+        lua.open_libraries(sol::lib::base, sol::lib::package);
+        try {
+            sol::table config = lua.script_file(path).get<sol::table>();
+            for (auto& [key, setter] : mappings)
+                if (config[key].valid())
+                    setter(config[key]);
+            std::cout << "lua-settings was loaded: " << path << "\n";
+        } catch (const sol::error& e) {
+            std::cerr << "ERROR: lua-settings: " << e.what() << "\n";
+        }
+    }
+
+private:
+    std::vector<std::pair<std::string, std::function<void(sol::object)>>> mappings;
+
+    template <typename U>
+    void addMapping(const std::string& key, Setting<U>& field) {
+        mappings.push_back({key, [&field](sol::object obj) {
+            field.setFromLua(obj);
+        }});
+    }
+
+    void initMappings() {
+        addMapping("target_fps", target_fps);
+        addMapping("project_name", project_name);
+        addMapping("window_size", window_size);
+        addMapping("resizable", resizable);
+    }
 };
 
 }
