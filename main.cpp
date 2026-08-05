f59a449a0e5f482c90d003bc68db7400cb4d2b5c
Capibara <barbashin.redkern@gmail.com>
2026-08-06 02:52:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <raylib.h>
-
 #include "settings.hpp"
 
 calamit::Settings settings;
 
 int main() {
-    if (settings.getResizable())
-        SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    settings.loadFromLua("lua/settings.lua");
     
-    auto winsize = settings.getWindowSize();
-    InitWindow(winsize.x, winsize.y, 
-               settings.getProjectName().c_str());
-    SetTargetFPS(settings.getTargetFps());
+    if (settings.resizable.get())
+        SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    Vector2 winsize = settings.window_size.get();
+    InitWindow(static_cast<int>(winsize.x), static_cast<int>(winsize.y),
+               settings.project_name.get().c_str());
+    SetTargetFPS(settings.target_fps.get());
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        int y = 30;
+        int lineHeight = 30;
+
+        int currentWidth = GetScreenWidth();
+        int currentHeight = GetScreenHeight();
+
+        DrawText(TextFormat("Project: %s", settings.project_name.get().c_str()), 20, y, 20, DARKGRAY);
+        y += lineHeight;
+        DrawText(TextFormat("Target FPS: %d", settings.target_fps.get()), 20, y, 20, DARKGRAY);
+        y += lineHeight;
+        DrawText(TextFormat("Window size: %d x %d", currentWidth, currentHeight), 20, y, 20, DARKGRAY);
+        y += lineHeight;
+        DrawText(TextFormat("Resizable: %s", settings.resizable.get() ? "Yes" : "No"), 20, y, 20, DARKGRAY);
+
         DrawFPS(10, 10);
         EndDrawing();
     }
