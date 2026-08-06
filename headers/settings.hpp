3939b084967ce3b4fc7ccf1934b0dc5dcf652214
Capibara <barbashin.redkern@gmail.com>
2026-08-06 03:01:16 +0300

    Update settings.hpp

--- headers/settings.hpp
+++ headers/settings.hpp
@@ -4,10 +4,10 @@
 #include <string>
 #include <vector>
 #include <functional>
+#include <unordered_map>
 #include <raylib.h>
 #include <sol/sol.hpp>
 #include <iostream>
-
 #include "luaConverter.hpp"
 
 namespace calamit {
@@ -40,28 +40,37 @@ public:
     Setting<Vector2> window_size{{800.0f, 600.0f}};
     Setting<bool> resizable{false};
 
+    void setValue(const std::string& key, sol::object value) {
+        auto it = mappings_map.find(key);
+        if (it != mappings_map.end())
+            it->second(value);
+        else
+            std::cerr << "Warning: unknown setting key: " << key << "\n";
+    }
+
     void loadFromLua(const std::string& path) {
         sol::state lua;
         lua.open_libraries(sol::lib::base, sol::lib::package);
+
+        lua.set_function("set_setting", [this](const std::string& key, sol::object value) {
+            this->setValue(key, value);
+        });
+
         try {
-            sol::table config = lua.script_file(path).get<sol::table>();
-            for (auto& [key, setter] : mappings)
-                if (config[key].valid())
-                    setter(config[key]);
-            std::cout << "lua-settings was loaded: " << path << "\n";
+            lua.script_file(path);
+            std::cout << "Lua settings script executed: " << path << "\n";
         } catch (const sol::error& e) {
             std::cerr << "ERROR: lua-settings: " << e.what() << "\n";
         }
     }
 
 private:
-    std::vector<std::pair<std::string, std::function<void(sol::object)>>> mappings;
-
+    std::unordered_map<std::string, std::function<void(sol::object)>> mappings_map;
     template <typename U>
     void addMapping(const std::string& key, Setting<U>& field) {
-        mappings.push_back({key, [&field](sol::object obj) {
+        mappings_map[key] = [&field](sol::object obj) {
             field.setFromLua(obj);
-        }});
+        };
     }
 
     void initMappings() {
