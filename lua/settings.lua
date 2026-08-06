5a164ba726449dfd0926991d2716f169433560fd
Capibara <barbashin.redkern@gmail.com>
2026-08-06 03:09:15 +0300

    Update settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,4 +1,6 @@
-set_setting("project_name", "Calamit Engine (Lua)")
-set_setting("target_fps", 144)
-set_setting("window_size", {1024, 765})
-set_setting("resizable", true)
+Settings = {
+    project_name = "Calamit Engine (Lua)",
+    target_fps = 144,
+    window_size = {1024, 765},
+    resizable = true
+}
