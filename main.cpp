93cfcc8e9013e6b5a1be7cfc4e8e9742fe0465e2
Capibara <barbashin.redkern@gmail.com>
2026-08-25 02:35:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,56 @@
 #include "raylib.h"
+#include "settings.hpp"
 
-extern "C" {
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+int windowWidth = 800;
+int windowHeight = 600;
+int targetFPS = 60;
+bool windowResizable = false;
+std::string windowTitle = "Calamit";
+
+void registerSettings() {
+    ADD_SETTING(windowWidth);
+    ADD_SETTING(windowHeight);
+    ADD_SETTING(targetFPS);
+    ADD_SETTING(windowResizable);
+    ADD_SETTING(windowTitle);
 }
 
 int main() {
-    InitWindow(800, 600, "Test");
-    CloseWindow();
+    registerSettings();
+
     lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    lua_register(L, "set_setting", l_set_setting);
+    lua_register(L, "importSettings", l_importSettings);
+
+    if (luaL_dofile(L, "config.lua") != LUA_OK) {
+        const char* err = lua_tostring(L, -1);
+        fprintf(stderr, "Config warning: %s\n", err ? err : "(unknown)");
+        lua_pop(L, 1);
+    }
+
+    SetConfigFlags(windowResizable ? FLAG_WINDOW_RESIZABLE : 0);
+    InitWindow(windowWidth, windowHeight, windowTitle.c_str());
+    SetTargetFPS(targetFPS);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText(("Title: " + windowTitle).c_str(), 10, 10, 20, BLACK);
+        char buf[64];
+        sprintf(buf, "Size: %dx%d", windowWidth, windowHeight);
+        DrawText(buf, 10, 35, 20, BLACK);
+        sprintf(buf, "Target FPS: %d", targetFPS);
+        DrawText(buf, 10, 60, 20, BLACK);
+        DrawText(windowResizable ? "Resizable: ON" : "Resizable: OFF", 10, 85, 20, BLACK);
+        DrawFPS(10, 110);
+
+        EndDrawing();
+    }
+
     lua_close(L);
+    CloseWindow();
     return 0;
 }
-
-// #include <iostream>
-// #include <raylib.h>
-// #include "settings.hpp"
-
-// calamit::Settings settings;
-
-// int main() {
-//     settings.loadFromLua("lua/settings.lua");
-    
-//     if (settings.resizable.get())
-//         SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-
-//     Vector2 winsize = settings.window_size.get();
-//     InitWindow(static_cast<int>(winsize.x), static_cast<int>(winsize.y),
-//                settings.project_name.get().c_str());
-//     SetTargetFPS(settings.target_fps.get());
-
-//     while (!WindowShouldClose()) {
-//         BeginDrawing();
-//         ClearBackground(RAYWHITE);
-
-//         int y = 30;
-//         int lineHeight = 30;
-
-//         int currentWidth = GetScreenWidth();
-//         int currentHeight = GetScreenHeight();
-
-//         DrawText(TextFormat("Project: %s", settings.project_name.get().c_str()), 20, y, 20, DARKGRAY);
-//         y += lineHeight;
-//         DrawText(TextFormat("Target FPS: %d", settings.target_fps.get()), 20, y, 20, DARKGRAY);
-//         y += lineHeight;
-//         DrawText(TextFormat("Window size: %d x %d", currentWidth, currentHeight), 20, y, 20, DARKGRAY);
-//         y += lineHeight;
-//         DrawText(TextFormat("Resizable: %s", settings.resizable.get() ? "Yes" : "No"), 20, y, 20, DARKGRAY);
-
-//         DrawFPS(10, 10);
-//         EndDrawing();
-//     }
-
-//     CloseWindow();
-//     return 0;
-// }
