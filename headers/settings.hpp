3a74c43acf20f7e3ee19931a0ad3d3da55db4c33
Capibara <barbashin.redkern@gmail.com>
2026-08-25 22:38:35 +0300

    Update settings.hpp

--- headers/settings.hpp
+++ headers/settings.hpp
@@ -12,6 +12,7 @@ extern "C" {
 
 class SettingsRegistry {
     std::unordered_map<std::string, std::function<void(lua_State*, int)>> setters;
+    std::unordered_map<std::string, std::function<void(lua_State*)>> getters;
 public:
     template<typename T>
     void add(const char* name, T* var) {
@@ -31,6 +32,21 @@ public:
                 static_assert(sizeof(T) == 0, "Unsupported type");
             }
         };
+        getters[name] = [var](lua_State* L) {
+            if constexpr (std::is_same_v<T, int>) {
+                lua_pushinteger(L, *var);
+            } else if constexpr (std::is_same_v<T, float>) {
+                lua_pushnumber(L, *var);
+            } else if constexpr (std::is_same_v<T, double>) {
+                lua_pushnumber(L, *var);
+            } else if constexpr (std::is_same_v<T, bool>) {
+                lua_pushboolean(L, *var);
+            } else if constexpr (std::is_same_v<T, std::string>) {
+                lua_pushstring(L, var->c_str());
+            } else {
+                static_assert(sizeof(T) == 0, "Unsupported type");
+            }
+        };
     }
 
     bool set(const std::string& name, lua_State* L, int idx) {
@@ -41,19 +57,37 @@ public:
         }
         return false;
     }
+
+    bool get(const std::string& name, lua_State* L) {
+        auto it = getters.find(name);
+        if (it != getters.end()) {
+            it->second(L);
+            return true;
+        }
+        return false;
+    }
 };
 
 inline SettingsRegistry g_settings;
 #define ADD_SETTING(var) g_settings.add(#var, &var)
 
-inline int l_set_setting(lua_State* L) {
+// ---- Lua-функции для таблицы Settings ----
+static int l_settings_set(lua_State* L) {
     const char* name = luaL_checkstring(L, 1);
     if (g_settings.set(name, L, 2))
         return 0;
     return luaL_error(L, "Unknown setting: %s", name);
 }
 
-inline int l_importSettings(lua_State* L) {
+static int l_settings_get(lua_State* L) {
+    const char* name = luaL_checkstring(L, 1);
+    if (g_settings.get(name, L))
+        return 1;
+    lua_pushnil(L);
+    return 1;
+}
+
+static int l_settings_import(lua_State* L) {
     const char* path = luaL_checkstring(L, 1);
     if (luaL_dofile(L, path) != LUA_OK) {
         const char* err = lua_tostring(L, -1);
@@ -61,4 +95,15 @@ inline int l_importSettings(lua_State* L) {
         lua_pop(L, 1);
     }
     return 0;
+}
+
+inline void register_settings_table(lua_State* L) {
+    lua_newtable(L);
+    lua_pushcfunction(L, l_settings_set);
+    lua_setfield(L, -2, "set");
+    lua_pushcfunction(L, l_settings_get);
+    lua_setfield(L, -2, "get");
+    lua_pushcfunction(L, l_settings_import);
+    lua_setfield(L, -2, "import");
+    lua_setglobal(L, "Settings");
 }
