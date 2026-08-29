5be22bcafd343b5c56c46ad9c5b3c682fc962fb2
Capibara <barbashin.redkern@gmail.com>
2026-08-29 19:07:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,30 +9,19 @@ SETTINGS_PUSH(bool, windowResizable, false);
 SETTINGS_PUSH(std::string, windowTitle, "Calamit");
 
 int main() {
-
     lua_State* L = init_lua();
 
     SetConfigFlags(windowResizable ? FLAG_WINDOW_RESIZABLE : 0);
     InitWindow(windowWidth, windowHeight, windowTitle.c_str());
     SetTargetFPS(targetFPS);
 
-    call_onload_all(L);
+    call_tree_update(L);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        call_ondraw_all(L);
-
-        char buf[64];
-        DrawText(("Title: " + windowTitle).c_str(), 10, 10, 20, BLACK);
-        sprintf(buf, "Size: %dx%d", windowWidth, windowHeight);
-        DrawText(buf, 10, 35, 20, BLACK);
-        sprintf(buf, "Target FPS: %d", targetFPS);
-        DrawText(buf, 10, 60, 20, BLACK);
-        DrawText(windowResizable ? "Resizable: ON" : "Resizable: OFF", 10, 85, 20, BLACK);
-        DrawFPS(10, 110);
-
+        call_tree_draw(L);
+        DrawFPS(10, 10);
         EndDrawing();
     }
 
